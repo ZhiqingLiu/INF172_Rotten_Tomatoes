0e6575a3f35f773fe5225ced7fce0e57617cad1b
Christy <125519650+ZhiqingLiu@users.noreply.github.com>
2024-05-24 21:58:54 -0700

    Update db.py

--- db.py
+++ db.py
@@ -3,11 +3,11 @@ from sqlalchemy import create_engine
 
 engine_mysql = create_engine('mysql+mysqlconnector://root:12345678@localhost:3306/Movie_database')
 
-# SQLite database connection (creates the file if it doesn't exist)
+# SQLite database connection
 engine_sqlite = create_engine('sqlite:///movies.db')  # This will create a 'movies.db' SQLite file in the current directory
 
 # Read data from MySQL
-query = "SELECT * FROM movies"  # Adjust this query based on your specific requirements
+query = "SELECT * FROM movies"  
 data = pd.read_sql(query, engine_mysql)
 
 # Write data to SQLite
